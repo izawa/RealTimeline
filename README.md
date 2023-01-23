e802b828ae245b504ef6ffab6beb24e02a09a73c
Yukimitsu Izawa <you@example.com>
2023-01-23 14:08:27 +0900

    initial version

---
+++ README.md
@@ -0,0 +1,8 @@
+# RealTimeline
+A chrome extension which forces Twitter timeline into time series.
+
+# How To Use
+1. Open 'chrome://extensions'
+2. Enable developer mode
+3. click load unpacked
+4. select 'extension' directory
---
+++ extension/content.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", main, false);
+
+function main(e) {
+    if(location.hostname === "twitter.com") {
+	const jsInitCheckTimer = setInterval(jsLoaded, 1000);
+	function jsLoaded() {
+	    var elements = document.querySelectorAll('span');
+	    var fe = Array.from(elements).filter((element)=>{return element.textContent === 'フォロー中'; });
+            if (fe != null) {
+		clearInterval(jsInitCheckTimer);
+		fe[0].click();
+	    }}}};
